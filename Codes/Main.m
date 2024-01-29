f2fbcc0bac5877f533944c4a2d0b6ef100965569
SchHNP <157456091+SchHNP@users.noreply.github.com>
2024-01-29 18:50:08 +0800

    Update Main.m

--- Codes/Main.m
+++ Codes/Main.m
@@ -1,27 +1,24 @@
 
-
 %%
-[bic, aic] = mvar_model_order(Data_EEG', 2, 20);
-ADTF_Sub =  ADTF(Data_EEG, ADTD_freqLow, ADTD_freqUpper, bic, fs, 1);
-
-
-%%
-Function_ADTF_Degrees_AutoP(ADTF_Sub(:, :, :, ADTD_freq(ADTD_freq_i, 1):ADTD_freq(ADTD_freq_i, 2)), Sub_Name_S, Output_file_PathSub);
+Data_EEG = rand(19, 125000);
+[bic, aic] = mvar_model_order(Data_EEG, 2, 20);
+fs = 250;
+ADTF_Sub =  ADTF(Data_EEG', 8, 13, bic, fs, 1);
 
 
 %%
-rand()
-Bold_Y = ROI_GignalSub(ROI_GignalSub_i, :);
-Bold_X = ROI_GignalSub(ROI_GignalSub_j, :);
-PsySignal = Psy_Sub;
+Function_ADTF_Degrees_AutoP(ADTF_Sub, 'Subname', 'OutputfilePath');
+
+
+%%
+Bold_Y = rand(250, 1);
+Bold_X = rand(250, 1);
+PsySignal = rand(250, 1);
 beta = function_PPI_Version(Bold_Y, Bold_X, PsySignal, 2);
-PPI_MatSubB1(ROI_GignalSub_i, ROI_GignalSub_j) = beta(1);
 
 
 %%
+X = rand(30, 50);
+Y = rand(30, 40);
+[R, T, p, df] = dcor_uc(X, Y);
 
-Bold_Y = ROI_GignalSub(ROI_GignalSub_i, :);
-Bold_X = ROI_GignalSub(ROI_GignalSub_j, :);
-PsySignal = Psy_Sub;
-beta = function_PPI_Version(Bold_Y, Bold_X, PsySignal, 2);
-PPI_MatSubB1(ROI_GignalSub_i, ROI_GignalSub_j) = beta(1);
